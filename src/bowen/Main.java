83356b5e42c1e6d69c174778a794e140234dbd8b
Chris Bowen <50932159+mmxchris@users.noreply.github.com>
2020-02-01 17:14:23 -0500

    Added message to be removed

--- src/bowen/Main.java
+++ src/bowen/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
         System.out.println("Hello, my name is Chris.");
         System.out.println("Hello, my name is Guillermo");
+        System.out.println("This need to be removed");
     }
 }
